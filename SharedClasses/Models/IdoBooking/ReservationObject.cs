6347cf9bc7edce598d807c525fa542e251361c19
posith <posith@gmail.com>
2025-10-07 20:20:33 +0200

    mały cleanup - Zmieniono lokalizację klas DTO dla Ido - wrzuciłem pod katalogo Models\idobooking

--- SharedClasses/Models/IdoBooking/ReservationObject.cs
+++ SharedClasses/Models/IdoBooking/ReservationObject.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RentoomBooking.SharedClasses.Models
+namespace RentoomBooking.SharedClasses.Models.IdoBooking
 {
 
 
@@ -97,9 +97,9 @@ namespace RentoomBooking.SharedClasses.Models
 
         public int getDuration()
         {
-            DateTime dateStart = DateTime.ParseExact(this.dateFrom, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
-            DateTime dateEnd = DateTime.ParseExact(this.dateTo, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
-            var days = (int)dateEnd.Subtract(dateStart).Days;
+            DateTime dateStart = DateTime.ParseExact(dateFrom, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
+            DateTime dateEnd = DateTime.ParseExact(dateTo, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
+            var days = dateEnd.Subtract(dateStart).Days;
             return days + 1;
 
 
--- SharedClasses/Models/RentoomReservation.cs
+++ SharedClasses/Models/RentoomReservation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RentoomBooking.SharedClasses.Models.IdoBooking;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
