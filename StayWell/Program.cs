2ab1e427290c499d95f52c267706a09c47a628e6
mk-rnt <mateusz.kujawa@rentoom.pl>
2025-10-08 04:07:11 +0200

    Dodano: wczytywanie zdjecia apartamentu Poprawiono zarządzanie Stanami i odpytaniami. +poprawki wizualne

--- StayWell/Program.cs
+++ StayWell/Program.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Logging;
 using RentoomBooking.SharedClasses;
 using RentoomBooking.StayWell.Services;
 using System.Text.Json;
@@ -20,16 +17,16 @@ namespace RentoomBooking.StayWell
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped<ReservationState>();
+            builder.Services.AddScoped<MediaState>();
 
             var apiBase = builder.Configuration["ApiBaseUrl"] ?? "/api/";
 
             builder.Services.AddHttpClient("FunctionsApi", c =>
             {
-                c.BaseAddress = new Uri(apiBase);
+                c.BaseAddress = new Uri("http://localhost:7238"+apiBase);
             });
 
             builder.Services.AddScoped<BackendApi>();
-            builder.Services.AddScoped<ReservationService>();
 
             builder.Services.AddSingleton(new JsonSerializerOptions(JsonSerializerDefaults.Web));
             
