9ecf2e30194bfca599ea69ee835d2193477f1758
michab1332 <bonowiczmichal1332@gmail.com>
2025-10-13 20:31:14 +0200

    feat: change way of getting img data

--- RentoomBookingWeb/wwwroot/js/mapInterop.js
+++ RentoomBookingWeb/wwwroot/js/mapInterop.js
@@ -9,18 +9,19 @@ window.leafletMap = {
         window._currentMap = map;
         window._defaultView = { lat: lat, lng: lng, zoom: zoom };
 
-        map.on('click', function(e) {
+        map.on('click', function() {
             map.flyTo([window._defaultView.lat, window._defaultView.lng], window._defaultView.zoom);
         });
     },
 
     addMarkers: function (markers) {
         if (!window._currentMap) return;
-
         if (!Array.isArray(markers)) {
-            console.error("addMarkers: markers is not an array", markers);
+            console.error("markers is not an array", markers);
             return;
         }
+
+        const mediaCache = new Map();
 
         markers.forEach(m => {
             const lat = parseFloat(m.lat);
@@ -32,18 +33,40 @@ window.leafletMap = {
             if (m.popup)
                 marker.bindPopup(m.popup);
 
-            marker.on('click', function () {
+            marker.on('click', async function () {
                 const zoomLevel = window._currentMap.getZoom() < 16 ? 16 : window._currentMap.getZoom();
                 window._currentMap.flyTo([lat, lng], zoomLevel);
 
-                if (window.DotNet && m.id) {
-                    DotNet.invokeMethodAsync('RentoomBookingWeb', 'OnMarkerClicked', m.id);
+                if (m.objRef && m.id) {
+                    if (mediaCache.has(m.id)) {
+                        const cachedUrl = mediaCache.get(m.id);
+                        updatePopupImage(m.id, cachedUrl);
+                        return;
+                    }
+
+                    try {
+                        const imageUrl = await m.objRef.invokeMethodAsync('OnMarkerClicked', m.id);
+                        if (imageUrl) {
+                            mediaCache.set(m.id, imageUrl);
+                            updatePopupImage(m.id, imageUrl);
+                        }
+                    } catch (err) {
+                        console.error('Error:', err);
+                    }
                 }
             });
-
-            marker.on('popupclose', function () {
-                window._currentMap.flyTo([window._defaultView.lat, window._defaultView.lng], window._defaultView.zoom);
-            });
         });
+
+        function updatePopupImage(id, url) {
+            const popupEl = document.getElementById(`popup-${id}`);
+            if (!popupEl) return;
+
+            const imgContainer = popupEl.querySelector("div");
+            if (imgContainer) {
+                imgContainer.innerHTML = `
+                    <img src="${url}" style="height: 130px; width: 250px; object-fit: cover; border-radius: .5rem;" />
+                `;
+            }
+        }
     }
 };
