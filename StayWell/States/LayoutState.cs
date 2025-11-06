f70f327352fb62def1aa964b41ac3eae3888f7bb
mk-rnt <mateusz.kujawa@rentoom.pl>
2025-11-06 23:45:45 +0100

    Dodano nowe funkcje i poprawki UI
    Wprowadzono nowy parametr `RenderFragment` w `Button.razor` dla elastycznego renderowania. Zaktualizowano style w `Description.razor.css` i `PageInfo.razor.css`. Dodano `LayoutState` do zarządzania nagłówkiem i stopką w `MainLayout.razor`. Utworzono klasę `Guest` w `Guest.cs`. Dodano nowe strony: rejestracji (`RegistrationPage.razor`) i regulaminu (`TermsPage.razor`) wraz z odpowiednimi stylami. Dodano `LayoutState` jako singleton w `Program.cs`. Dodano nowe pliki graficzne: `handshake.svg` i `terms-page.png`.

---
+++ StayWell/States/LayoutState.cs
@@ -0,0 +1,37 @@
+﻿namespace RentoomBooking.StayWell.States
+{
+    public class LayoutState
+    {
+        private bool _showFooter = true;
+        private bool _showHeader = true;
+
+        public bool ShowFooter
+        {
+            get => _showFooter;
+            set
+            {
+                if(_showFooter != value)
+                {
+                    _showFooter = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+
+        public bool ShowHeader
+        {
+            get => _showHeader;
+            set
+            {
+                if (_showHeader != value)
+                {
+                    _showHeader = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+
+        public event Action? OnChange;
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
