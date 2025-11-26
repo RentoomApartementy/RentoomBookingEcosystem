93c6b132aae646309a66123739ebfdf62e3fccbf
posith <posith@gmail.com>
2025-11-26 16:09:24 +0100

    Added Reservations

---
+++ Api/AddReservationFunction.cs
@@ -0,0 +1,76 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RentoomBooking.SharedClasses.Models.IdoBooking;
+using RentoomBooking.SharedClasses.Models.IdoBooking.ReservationManagement;
+using RentoomBooking.SharedClasses.Services;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RentoomBooking.Api;
+
+public class AddReservationFunction
+{
+    private readonly IdoSellService _idoSellService;
+    private readonly ILogger<AddReservationFunction> _logger;
+
+    public AddReservationFunction(IdoSellService idoSellService, ILogger<AddReservationFunction> logger)
+    {
+        _idoSellService = idoSellService ?? throw new ArgumentNullException(nameof(idoSellService));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    [Function("AddReservationToIdoSell")]
+    public async Task<HttpResponseData> AddReservationToIdoSell(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "ido/reservations")] HttpRequestData req)
+    {
+        _logger.LogInformation("AddReservationToIdoSell started at: {time}", DateTime.UtcNow);
+        var response = req.CreateResponse();
+
+        try
+        {
+            string body;
+            using (var reader = new StreamReader(req.Body))
+            {
+                body = await reader.ReadToEndAsync().ConfigureAwait(false);
+            }
+
+            if (string.IsNullOrWhiteSpace(body))
+            {
+                response.StatusCode = HttpStatusCode.BadRequest;
+                await response.WriteStringAsync("Request body is empty.");
+                return response;
+            }
+           
+            var request = JsonConvert.DeserializeObject<ReservationAddParams>(body);
+
+            if (request == null || request.Reservations.Count == 0)
+            {
+                response.StatusCode = HttpStatusCode.BadRequest;
+                await response.WriteStringAsync("Provide at least one reservation in the params.reservations array.");
+                return response;
+            }
+
+            var result = await _idoSellService.AddReservationsAsync(request.Reservations);
+
+            response.StatusCode = HttpStatusCode.OK;
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            await response.WriteStringAsync(JsonConvert.SerializeObject(result));
+            return response;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error during AddReservationToIdoSell.");
+            response.StatusCode = HttpStatusCode.InternalServerError;
+            await response.WriteStringAsync("Internal server error.");
+            return response;
+        }
+        finally
+        {
+            _logger.LogInformation("AddReservationToIdoSell finished at: {time}", DateTime.UtcNow);
+        }
+    }
+}
