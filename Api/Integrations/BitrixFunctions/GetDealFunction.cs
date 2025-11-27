68774e417ddecbcdaf11e9e1ffa6a3bc41cf090f
posith <posith@gmail.com>
2025-11-27 14:33:09 +0100

    Added Bitrix main service and methods. todo: add specific methods for crm.leads.add bitrxi webhook

---
+++ Api/Integrations/BitrixFunctions/GetDealFunction.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using RentoomBooking.SharedClasses.Integrations.Bitrix.Models;
+using RentoomBooking.SharedClasses.Integrations.Bitrix.Services;
+
+namespace RentoomBooking.Api.Integrations.BitrixFunctions;
+
+public class GetDealFunction
+{
+    private readonly ILogger<GetDealFunction> _logger;
+    private readonly BitrixService _bitrixService;
+
+    public GetDealFunction(ILogger<GetDealFunction> logger, BitrixService bitrixService)
+    {
+        _logger = logger;
+        _bitrixService = bitrixService;
+    }
+
+    [Function("GetDealFunction")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "bitrix/deals/{id}")] HttpRequest req, int id)
+    {
+
+        try
+        {
+
+            var dealFieldsDef = await _bitrixService.DownloadDealFieldsDefinitionJsonAsync();
+            var dealdata = await _bitrixService.DownloadDealDetailsJsonAsync(id, dealFieldsDef);
+
+            var customerFieldsDef = await _bitrixService.DownloadCustomerFieldsDefinitionJsonAsync();
+
+            var customerId = Convert.ToInt16(dealdata.DealData.FirstOrDefault(m => m.FieldID == "CONTACT_ID")?.Value);
+
+            var customerdata = new BitrixDealData();
+            if (customerId > 0) 
+
+            customerdata = await _bitrixService.DownloadContactDetailsJsonAsync(customerId, customerFieldsDef);
+
+            var deal = new BitrixDealForm
+            {
+                CustomerInfo = customerdata,
+                DealForm = dealdata,
+            };
+
+            return new OkObjectResult(deal);
+        }
+        catch (Exception ex)
+        {
+            return new NotFoundObjectResult(ex.Message);
+        }
+
+    }
+}
