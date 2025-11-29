41a92ce02c2dcc947ec63d685157854d9d42c9ee
michab1332 <bonowiczmichal1332@gmail.com>
2025-11-29 22:56:59 +0100

    feat/sw-189

---
+++ SharedClasses/Database/PostgresBookingDbContextFactory.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace RentoomBooking.SharedClasses.Database
+{
+    public class PostgresBookingDbContextFactory : IDesignTimeDbContextFactory<PostgresBookingDbContext>
+    {
+        public PostgresBookingDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PostgresBookingDbContext>();
+
+            // Twój lokalny connection string do Postgresa
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=RentoomBookingTest;Username=postgres;Password=123");
+
+            return new PostgresBookingDbContext(optionsBuilder.Options);
+        }
+    }
+}
