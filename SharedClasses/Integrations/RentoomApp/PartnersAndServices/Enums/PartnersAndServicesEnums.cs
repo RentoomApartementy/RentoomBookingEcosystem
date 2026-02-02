8b03e90cc4eb305b7501ee92c70b7a3110f79f93
posith <posith@gmail.com>
2026-02-02 15:56:39 +0100

    Dodano Enums i EF Klasy dla integracji z RentoomApp

---
+++ SharedClasses/Integrations/RentoomApp/PartnersAndServices/Enums/PartnersAndServicesEnums.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentoomBooking.SharedClasses.Integrations.RentoomApp.PartnersAndServices.Enums
+{
+    public enum PartnerStatus
+    {
+        Draft,
+        Active,
+        Archived
+    }
+
+    public enum PartnerType
+    {
+        Restaurant,
+        Spa,
+        Tour,
+        Other
+    }
+
+    public enum PartnerServiceStatus
+    {
+        Draft,
+        Active,
+        Archived
+    }
+
+    public enum PartnerServiceCategory
+    {
+        [Display(Name = "Śniadania")]
+        Breakfast,
+        [Display(Name = "Obiady")]
+        Lunch,
+        [Display(Name = "Kolacje")]
+        Dinner,
+        [Display(Name = "Masaż")]
+        Massage,
+        [Display(Name = "Wycieczka")]
+        Trip,
+        [Display(Name = "Inne")]
+        Other
+    }
+
+    public enum PartnerServicePricingModel
+    {
+        [Display(Name = "Na osobę na dzień")]
+        PerPersonPerDay,
+        [Display(Name = "Na apartament na dzień")]
+        PerApartmentPerDay,
+        [Display(Name = "Na pobyt")]
+        PerStay,
+        [Display(Name = "Jednorazowo")]
+        OneTime
+    }
+
+    public enum PartnerServiceDiscountType
+    {
+        [Display(Name = "Brak")]
+        None,
+        [Display(Name = "Procent")]
+        Percent,
+        [Display(Name = "Stała Kwota")]
+        FixedAmount
+    }
+
+    public enum PartnerServiceBannerPlacementType
+    {
+        [Display(Name = "Staywell")]
+        StayWell,
+        [Display(Name = "Instagram")]
+        Instagram,
+        [Display(Name = "Facebook")]
+        Facebook,
+        [Display(Name = "Whatsapp")]
+        WhatsApp
+    }
+
+    public enum PartnerServiceTargetType
+    {
+        Apartment
+    }
+}
---
+++ SharedClasses/Integrations/RentoomApp/PartnersAndServices/RappPartnersDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentoomBooking.SharedClasses.Integrations.RentoomApp.PartnersAndServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentoomBooking.SharedClasses.Integrations.RentoomApp.PartnersAndServices
+{
+    public class RappPartnersDBContext :DbContext
+    {
+        public DbSet<Partner> Partners { get; set; }
+        public DbSet<PartnerSupportedLanguage> PartnerSupportedLanguages { get; set; }
+        public DbSet<PartnerService> PartnerServices { get; set; }
+        public DbSet<PartnerServiceTranslation> PartnerServiceTranslations { get; set; }
+        public DbSet<MediaAsset> MediaAssets { get; set; }
+        public DbSet<PartnerServiceBanner> PartnerServiceBanners { get; set; }
+        public DbSet<PartnerServiceTarget> PartnerServiceTargets { get; set; }
+
+
+    }
+}
