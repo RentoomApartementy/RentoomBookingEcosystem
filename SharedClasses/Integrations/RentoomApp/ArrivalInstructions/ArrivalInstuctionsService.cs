c49f5add7b3a85df115e8e28b519e6917523ca65
posith <posith@gmail.com>
2026-02-13 14:08:04 +0100

    Maciej dev 12 feb (#134)
    * arrival instructions
    
    * arrival instructions service

---
+++ SharedClasses/Integrations/RentoomApp/ArrivalInstructions/ArrivalInstuctionsService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentoomBooking.SharedClasses.Integrations.RentoomApp.ArrivalInstructions
+{
+    public class ArrivalInstructionsService
+    {
+        private readonly IDbContextFactory<RappInstructionsDbContext> _dbContextFactory;
+
+        public ArrivalInstructionsService(IDbContextFactory<RappInstructionsDbContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<IReadOnlyList<ApartmentArrivalInstructionStepDTO>> GetArrivalInstructionStepsAsync(
+            int apartmentId,
+            CancellationToken cancellationToken = default)
+        {
+            await using var dbContext = await _dbContextFactory.CreateDbContextAsync(cancellationToken);
+
+            return await dbContext.ArrivalInstructions
+                .AsNoTracking()
+                .Where(step => step.ApartmentId == apartmentId)
+                .OrderBy(step => step.Sequence)
+                .Select(step => new ApartmentArrivalInstructionStepDTO
+                {
+                    Id = step.Id,
+                    ApartmentId = step.ApartmentId,
+                    Sequence = step.Sequence,
+                    Name = step.Name,
+                    Description = step.Description,
+                    ImageMediaAssetId = step.ImageMediaAssetId,
+                    ImageUrl = step.ImageUrl
+                })
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
---
+++ SharedClasses/Integrations/RentoomApp/ArrivalInstructions/RappInstructionsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentoomBooking.SharedClasses.Integrations.RentoomApp.Partners.Database;
+using RentoomBooking.SharedClasses.Integrations.RentoomApp.PartnersAndServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentoomBooking.SharedClasses.Integrations.RentoomApp.ArrivalInstructions
+{
+    public class RappInstructionsDbContext: DbContext
+    {
+        public RappInstructionsDbContext(DbContextOptions<RappInstructionsDbContext> options) : base(options) { }
+        public DbSet<ApartmentArrivalInstructionStep> ArrivalInstructions { get; set; }
+    }
+}
