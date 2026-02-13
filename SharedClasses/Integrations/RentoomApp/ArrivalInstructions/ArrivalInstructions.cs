96ef8fca73b10551b54467974014deaf4d38ddf6
posith <posith@gmail.com>
2026-02-13 14:02:02 +0100

    arrival instructions

---
+++ SharedClasses/Integrations/RentoomApp/ArrivalInstructions/ArrivalInstructions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentoomBooking.SharedClasses.Integrations.RentoomApp.ArrivalInstructions
+{
+    public class ApartmentArrivalInstructionStepDTO
+    {
+        public int Id { get; set; }
+        public int ApartmentId { get; set; }
+        public int Sequence { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int? ImageMediaAssetId { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+
+    public class ApartmentArrivalInstructionStep
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ApartmentId { get; set; }
+        public Apartment Apartment { get; set; } = null!;
+
+        [Required]
+        public int Sequence { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        public int? ImageMediaAssetId { get; set; }
+    }
+}
---
+++ SharedClasses/Integrations/RentoomApp/ArrivalInstructions/RappInstructionsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentoomBooking.SharedClasses.Integrations.RentoomApp.Partners.Database;
+using RentoomBooking.SharedClasses.Integrations.RentoomApp.PartnersAndServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentoomBooking.SharedClasses.Integrations.RentoomApp.ArrivalInstructions
+{
+    internal class RappInstructionsDbContext: DbContext
+    {
+        public RappInstructionsDbContext(DbContextOptions<RappInstructionsDbContext> options) : base(options) { }
+        public DbSet<ApartmentArrivalInstructionStep> ArivalInstructions { get; set; }
+    }
+}
