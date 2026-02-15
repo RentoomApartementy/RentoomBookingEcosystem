6f85571f0e866ceda38a818f02b7aacda2af9a53
posith <posith@gmail.com>
2026-02-15 12:39:51 +0100

    translacja upselli

--- SharedFrontend/Components/Shared/UpsellComponents/UpsellTextConfig.cs
+++ SharedFrontend/Components/Shared/UpsellComponents/UpsellTextConfig.cs
@@ -13,5 +13,31 @@ namespace RentoomBooking.SharedFrontend.Components.Shared.UpsellComponents
         public string DescriptionText { get; set; } = "Description";
         public string TermsTitleText { get; set; } = "Terms & Conditions";
         public string TermsDescriptionText { get; set; } = "Valid for the duration of your stay. Must present coupon code at time of service.";
+        public string LinkLabel { get; set; } = "Link";
+        public string DetailsText { get; set; } = "Details";
+        public string ShowVoucherCodeText { get; set; } = "Show voucher code";
+        public string YourCodeText { get; set; } = "Your code";
+        public string ManualCodeLabelText { get; set; } = "Code to enter manually";
+        public string VoucherQrAltText { get; set; } = "Voucher QR code";
+        public string QrUnavailableText { get; set; } = "QR unavailable";
+        public string EnlargeText { get; set; } = "Enlarge";
+        public string AddressText { get; set; } = "Address";
+        public string VoucherUsageTitleText { get; set; } = "Voucher usage";
+        public string VoucherStatusText { get; set; } = "Status";
+        public string VoucherUsedCountText { get; set; } = "Used";
+        public string VoucherRemainingText { get; set; } = "Remaining";
+        public string VoucherValidFromText { get; set; } = "Valid from";
+        public string VoucherValidToText { get; set; } = "Valid to";
+        public string VoucherNoLimitText { get; set; } = "No limit";
+        public string ShowInRestaurantText { get; set; } = "Show at restaurant";
+        public string VoucherCodeText { get; set; } = "Code";
+        public string VoucherOverlayHintText { get; set; } = "Show this code to staff to redeem the voucher.";
+
+        public string PriceLabel { get; set; } = "Price";
+        public string PricingPerPersonPerDayText { get; set; } = "person/day";
+        public string PricingPerApartmentPerDayText { get; set; } = "apartment/day";
+        public string PricingPerStayText { get; set; } = "stay";
+        public string PricingOneTimeText { get; set; } = "one-time";
+
     }
 }
