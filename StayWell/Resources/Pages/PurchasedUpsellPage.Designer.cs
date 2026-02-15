4c1d21048177916b7cdc70c336ce8e50a5f3a9e8
posith <posith@gmail.com>
2026-02-15 13:37:20 +0100

    zmiany jezyka nowy plik resx

--- StayWell/Resources/Pages/PurchasedUpsellPage.Designer.cs
+++ StayWell/Resources/Pages/PurchasedUpsellPage.Designer.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RentoomBookingWeb {
+namespace RentoomBooking.StayWell {
     using System;
     
     
