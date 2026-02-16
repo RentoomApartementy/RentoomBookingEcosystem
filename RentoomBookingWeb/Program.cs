17d0045d30972199070c9e10428ea755a48518d7
posith <posith@gmail.com>
2026-02-17 00:19:39 +0100

    dummy change to enforce to push to main

--- RentoomBookingWeb/Program.cs
+++ RentoomBookingWeb/Program.cs
@@ -69,6 +69,7 @@ namespace RentoomBookingWeb
                .GetPostgresConnectionString(builder.Configuration, "RentoomDbConnectionString", builder.Environment.IsDevelopment(), tempLogger);
                
 
+
             builder.Services.AddDbContextFactory<RappPartnersDBContext>(options =>
                 options.UseNpgsql(rentoomAppConnectionString));
 
