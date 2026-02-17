ebb5d36e5beef6af484336f687650c09c63a73aa
mk-rnt <mateusz.kujawa@rentoom.pl>
2026-02-17 09:37:51 +0100

    +https

--- StayWell/Program.cs
+++ StayWell/Program.cs
@@ -46,7 +46,7 @@ namespace RentoomBooking.StayWell
                 if (builder.HostEnvironment.IsDevelopment())
                 {
                     // Local dev:
-                    c.BaseAddress = new Uri("http://localhost:7238/api/");
+                    c.BaseAddress = new Uri("https://localhost:7238/api/");
                 }
                 else
                 {
