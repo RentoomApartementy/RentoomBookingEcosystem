833eb49d7f172cb26d8e948c5f038cdd118ee13d
mk-rnt <mateusz.kujawa@rentoom.pl>
2026-03-06 11:42:48 +0100

    Endpointy,funkcje i dto dla staywell

--- SharedClasses/Database/CustomerTermsRepository.cs
+++ SharedClasses/Database/CustomerTermsRepository.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using RentoomBooking.SharedClasses.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RentoomBooking.SharedClasses.Database
@@ -25,6 +26,39 @@ namespace RentoomBooking.SharedClasses.Database
         {
             await _context.CustomerAgreedTerms.AddRangeAsync(agreedTerms);
             await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<CustomerAgreedTermDto>> GetAgreedTermsByReservationAsync(Guid reservationGuid)
+        {
+            return await _context.CustomerAgreedTerms
+                .Where(t => t.ReservationGuid == reservationGuid)
+                .Include(t => t.TermsSource)
+                .Select(t => new CustomerAgreedTermDto
+                {
+                    TermsSourceId = t.TermsSourceId,
+                    Description = t.TermsSource.Description,
+                    AgreedAt = t.AgreedAt,
+                    IsRequired = t.TermsSource.IsRequired,
+                    IsAccepted = t.IsAccepted
+                })
+                .ToListAsync();
+        }
+
+        public async Task<bool> UpdateAgreedTermAsync(Guid reservationGuid, int termsSourceId, bool isAccepted)
+        {
+            var entity = await _context.CustomerAgreedTerms
+                .FirstOrDefaultAsync(t => t.ReservationGuid == reservationGuid && t.TermsSourceId == termsSourceId);
+
+            if (entity is null)
+            {
+                return false;
+            }
+
+            entity.IsAccepted = isAccepted;
+            entity.AgreedAt = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+            return true;
         }
     }
 }
