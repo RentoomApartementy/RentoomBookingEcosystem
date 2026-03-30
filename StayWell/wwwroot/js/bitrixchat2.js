0113b043eb5bebcc43b29b4088a1e03eb035c2ca
posith <posith@gmail.com>
2026-03-30 20:03:27 +0200

    bitrix - fix dla late loading.

--- StayWell/wwwroot/js/bitrixchat2.js
+++ StayWell/wwwroot/js/bitrixchat2.js
@@ -1,6 +1,9 @@
 window.bitrixInterop = {
     widget: null,
     initialized: false,
+    pendingCustomData: null,
+    pendingOpen: false,
+    openRetryTimer: null,
 
     init: function () {
         if (window.bitrixInterop.initialized) {
@@ -10,6 +13,7 @@ window.bitrixInterop = {
         window.bitrixInterop.initialized = true;
         window.addEventListener("onBitrixLiveChat", function (event) {
             window.bitrixInterop.widget = event.detail.widget;
+            window.bitrixInterop.flushPending();
         });
     },
 
@@ -19,27 +23,92 @@ window.bitrixInterop = {
         }
     },
 
+    normalizeCustomData: function (rawData) {
+        if (!rawData) {
+            return null;
+        }
+
+        if (typeof rawData === "string") {
+            try {
+                return JSON.parse(rawData);
+            } catch (error) {
+                console.error("Bitrix customData JSON parse error", error);
+                return null;
+            }
+        }
+
+        return rawData;
+    },
+
+    getWidget: function () {
+        if (window.bitrixInterop.widget) {
+            return window.bitrixInterop.widget;
+        }
+
+        if (window.BX && window.BX.LiveChatWidget) {
+            window.bitrixInterop.widget = window.BX.LiveChatWidget;
+            return window.bitrixInterop.widget;
+        }
+
+        return null;
+    },
+
+    flushPending: function () {
+        var w = window.bitrixInterop.getWidget();
+        if (!w) {
+            return false;
+        }
+
+        if (window.bitrixInterop.pendingCustomData && typeof w.setCustomData === "function") {
+            try {
+                w.setCustomData(window.bitrixInterop.pendingCustomData);
+                console.log("Bitrix customData ustawione");
+            } catch (error) {
+                console.error("Bitrix setCustomData error", error);
+            }
+        }
+
+        if (window.bitrixInterop.pendingOpen && typeof w.open === "function") {
+            w.open();
+            window.bitrixInterop.pendingOpen = false;
+            return true;
+        }
+
+        return false;
+    },
+
+    scheduleRetry: function () {
+        if (window.bitrixInterop.openRetryTimer) {
+            return;
+        }
+
+        var attemptsLeft = 40;
+        window.bitrixInterop.openRetryTimer = window.setInterval(function () {
+            attemptsLeft -= 1;
+
+            if (window.bitrixInterop.flushPending() || attemptsLeft <= 0) {
+                window.clearInterval(window.bitrixInterop.openRetryTimer);
+                window.bitrixInterop.openRetryTimer = null;
+            }
+        }, 250);
+    },
+
     openChat: function (readyJsonData) {
-        var tryOpen = function () {
-            var w = window.bitrixInterop.widget;
+        window.bitrixInterop.pendingCustomData = window.bitrixInterop.normalizeCustomData(readyJsonData);
+        window.bitrixInterop.pendingOpen = true;
 
-            if (w && typeof w.setCustomData === "function") {
-                w.setCustomData(readyJsonData);
-                w.open();
-                console.log("udalo sie zaladowac dane");
-            }
-            else {
-                if (w) {
-                    w.open();
-                }
-                console.warn("nie dalo sie zaladowac danych");
-            }
-        };
-
-        tryOpen();
+        if (!window.bitrixInterop.flushPending()) {
+            window.bitrixInterop.scheduleRetry();
+            console.warn("Bitrix widget jeszcze niegotowy, ponawiam probe otwarcia");
+        }
     },
 
     destroy: function () {
+        if (window.bitrixInterop.openRetryTimer) {
+            window.clearInterval(window.bitrixInterop.openRetryTimer);
+            window.bitrixInterop.openRetryTimer = null;
+        }
+
         const selectors = [".b24-widget-button-wrapper", 'iframe[src*="bitrix24"]', 'script[src*="bitrix24"]'];
         selectors.forEach(function (selector) {
             document.querySelectorAll(selector).forEach(function (element) {
@@ -47,5 +116,7 @@ window.bitrixInterop = {
             });
         });
         window.bitrixInterop.widget = null;
+        window.bitrixInterop.pendingCustomData = null;
+        window.bitrixInterop.pendingOpen = false;
     }
 };
