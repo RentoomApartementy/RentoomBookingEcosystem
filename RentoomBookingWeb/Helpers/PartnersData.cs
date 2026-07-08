bd8cfc7dae96f710df191e0890ea9d750609971c
michab1332 <bonowiczmichal1332@gmail.com>
2026-07-08 22:01:58 +0200

    partner slider home page

---
+++ RentoomBookingWeb/Helpers/PartnersData.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using RentoomBooking.SharedClasses.Models.RentoomBooking;
+
+namespace RentoomBookingWeb.Helpers
+{
+    public static class PartnersData
+    {
+        public static readonly List<PartnerItem> Partners = new()
+        {
+            new() { ImageUrl = "assets/images/partners/7.png", Label = "Logo Meltme Tender", Link = "https://meltmetender.pl/" },
+            new() { ImageUrl = "assets/images/partners/8.png", Label = "Logo Rolki", Link = "https://www.facebook.com/rolkitorun/" },
+            new() { ImageUrl = "assets/images/partners/9.png", Label = "Logo Grande Piccolo Pizza & Pasta", Link = "https://www.facebook.com/grandepiccolotorun/" },
+            new() { ImageUrl = "assets/images/partners/10.png", Label = "Logo ArtSplash", Link = "https://www.artsplash.pl/" },
+            new() { ImageUrl = "assets/images/partners/11.png", Label = "Logo OmniKaiser Patisserie", Link = "https://omnikaiser.pl/" },
+            new() { ImageUrl = "assets/images/partners/12.png", Label = "Logo Dom Pizzy i Tapas", Link = "https://www.facebook.com/dompizzyitapas/" },
+            new() { ImageUrl = "assets/images/partners/13.png", Label = "Logo Dom Pizzy i Tapas Bistro", Link = "https://www.facebook.com/dompizzyitapasszumana/" },
+            new() { ImageUrl = "assets/images/partners/14.png", Label = "Logo Monka Restauracja i Apartamenty", Link = "https://monkatorun.com/" },
+            new() { ImageUrl = "assets/images/partners/15.png", Label = "Logo Olender Centrum Sportu i Rekreacji", Link = "https://www.olender.info/" },
+            new() { ImageUrl = "assets/images/partners/16.png", Label = "Logo Ale fajne Taxi", Link = "https://alefajnetaxi.pl/" },
+            new() { ImageUrl = "assets/images/partners/17.png", Label = "Logo Dom Kłódek Escape Room", Link = "https://www.domklodek-torun.pl/" },
+            new() { ImageUrl = "assets/images/partners/18.png", Label = "Logo Bułka", Link = "https://www.bulka.torun.pl/" },
+            new() { ImageUrl = "assets/images/partners/19.png", Label = "Logo Baciare Bistro", Link = "https://www.facebook.com/BaciareBistro/" },
+            new() { ImageUrl = "assets/images/partners/20.png", Label = "Logo Haust", Link = "https://haust.com.pl/" },
+            new() { ImageUrl = "assets/images/partners/21.png", Label = "Logo Kart Place - Tor Kartingowy", Link = "https://torun-plaza.pl/tenant/kart-place/" },
+            new() { ImageUrl = "assets/images/partners/22.png", Label = "Logo Rolki", Link = "https://www.facebook.com/rolkitorun/" },
+            new() { ImageUrl = "assets/images/partners/23.png", Label = "Logo Baciare Bistro", Link = "https://www.facebook.com/BaciareBistro/" }
+        };
+    }
+}
---
+++ SharedClasses/Models/RentoomBooking/PartnerItem.cs
@@ -0,0 +1,9 @@
+namespace RentoomBooking.SharedClasses.Models.RentoomBooking
+{
+    public class PartnerItem
+    {
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Label { get; set; } = string.Empty;
+        public string? Link { get; set; }
+    }
+}
