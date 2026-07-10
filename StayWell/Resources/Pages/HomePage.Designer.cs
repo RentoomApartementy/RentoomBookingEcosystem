387516e6f072bfbffef69a27e639e53153eb40c0
mk-rnt <mateusz.kujawa@rentoom.pl>
2026-07-10 08:57:41 +0200

    tlumaczenia 2

--- StayWell/Resources/Pages/HomePage.Designer.cs
+++ StayWell/Resources/Pages/HomePage.Designer.cs
@@ -14,21 +14,21 @@ namespace RentoomBooking.StayWell.Resources.Pages {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class HomePage_pl_PL {
+    internal class HomePage {
         
         private static System.Resources.ResourceManager resourceMan;
         
         private static System.Globalization.CultureInfo resourceCulture;
         
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal HomePage_pl_PL() {
+        internal HomePage() {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("RentoomBooking.StayWell.Resources.Pages.HomePage_pl_PL", typeof(HomePage_pl_PL).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("RentoomBooking.StayWell.Resources.Pages.HomePage", typeof(HomePage).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -72,6 +72,12 @@ namespace RentoomBooking.StayWell.Resources.Pages {
         internal static string CheckIn {
             get {
                 return ResourceManager.GetString("CheckIn", resourceCulture);
+            }
+        }
+        
+        internal static string CheckInNow {
+            get {
+                return ResourceManager.GetString("CheckInNow", resourceCulture);
             }
         }
         
@@ -243,15 +249,15 @@ namespace RentoomBooking.StayWell.Resources.Pages {
             }
         }
         
-        internal static string TranslationDisclaimer {
-            get {
-                return ResourceManager.GetString("TranslationDisclaimer", resourceCulture);
-            }
-        }
-        
         internal static string WifiFooter {
             get {
                 return ResourceManager.GetString("WifiFooter", resourceCulture);
+            }
+        }
+        
+        internal static string TranslationDisclaimer {
+            get {
+                return ResourceManager.GetString("TranslationDisclaimer", resourceCulture);
             }
         }
     }
