6fc423a0689c67b2c950ef8c5114400286b2020a
michab1332 <bonowiczmichal1332@gmail.com>
2026-07-12 22:42:14 +0200

    feat: rwd

--- RentoomBookingWeb/Components/Features/Blog/Components/BlogHeaderSection.razor.cs
+++ RentoomBookingWeb/Components/Features/Blog/Components/BlogHeaderSection.razor.cs
@@ -12,7 +12,23 @@ public partial class BlogHeaderSection : ComponentBase
     internal Microsoft.Extensions.Localization.IStringLocalizer<RentoomBookingWeb.Blog> Localizer { get; set; } = default!;
 
     [Parameter] 
-    public required BlogPostListItem Item { get; set; } =  null!;
+    public BlogPostListItem? Item { get; set; }
+
+    protected override void OnInitialized()
+    {
+        if (Item is null)
+        {
+            Item = new BlogPostListItem
+            {
+                Title = "Toruń na weekend",
+                Excerpt = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride ...",
+                HeroImageUrl = "https://storagerentoombooking.blob.core.windows.net/blog-assets-prod/blog/2026/07/posts/2/7e2cffee696f493f86fb248e86bfaa23.webp",
+                Category = "WAKACJE",
+                Tags = new[] { "WAKACJE", "WOLNE" },
+                PublishedAtUtc = new System.DateTime(2026, 4, 24)
+            };
+        }
+    }
 
     private string BuildPostUrl(Guid publicId, string slug) 
         => $"{RouteService.GetLocalizedUrl("BlogPost")}/{publicId:D}/{slug}";
